bfd6da3d91b18ba53b081f7ade39030ab27b07ce
Mnibr <147657979+Mnibr@users.noreply.github.com>
2026-05-09 15:18:45 +0800

    修修补补
    更改了红石钟朝向的代码，使其能够朝上和朝下
    红石钟不再能作为红石导体

--- src/main/java/com/hexagram2021/redstoneclock/common/register/RCBlocks.java
+++ src/main/java/com/hexagram2021/redstoneclock/common/register/RCBlocks.java
@@ -20,12 +20,13 @@ import static com.hexagram2021.redstoneclock.RedstoneClock.MODID;
 public final class RCBlocks {
 	private static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
-	public static final RegistryObject<RedstoneClockBlock> REDSTONE_CLOCK = REGISTER.register("redstone_clock", () -> new RedstoneClockBlock(
-			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(1.5F)
-					.lightLevel(blockState -> blockState.getValue(RedstoneClockBlock.POWERED) ? 7 : 0)
-					.sound(SoundType.METAL).pushReaction(PushReaction.BLOCK)
-					.isValidSpawn((BlockState blockState, BlockGetter level, BlockPos blockPos, EntityType<?> entityType) -> false)
-	));
+    public static final RegistryObject<RedstoneClockBlock> REDSTONE_CLOCK = REGISTER.register("redstone_clock", () -> new RedstoneClockBlock(
+            BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(1.5F)
+                    .lightLevel(blockState -> blockState.getValue(RedstoneClockBlock.POWERED) ? 7 : 0)
+                    .sound(SoundType.METAL).pushReaction(PushReaction.BLOCK)
+                    .isValidSpawn((BlockState blockState, BlockGetter level, BlockPos blockPos, EntityType<?> entityType) -> false)
+                    .isRedstoneConductor((state, level, pos) -> false)
+    ));
 	public static final RegistryObject<PulseDividerBlock> PULSE_DIVIDER = REGISTER.register("pulse_divider", () -> new PulseDividerBlock(
 			BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(1.5F)
 					.sound(SoundType.METAL).pushReaction(PushReaction.BLOCK)
